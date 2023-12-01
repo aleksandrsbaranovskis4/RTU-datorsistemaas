3560ca3a7285915149f2f5470ec03db53383d8ba
aleksandrsbaranovskis4 <aleksandrs.baranovskis_4@edu.rtu.lv>
2023-12-01 12:36:34 +0200

    hahaha

--- lietpr-aut-riki/4_uzdevums/231RDB065.py
+++ lietpr-aut-riki/4_uzdevums/231RDB065.py
@@ -1,7 +1,9 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup as bs
 import time
 from openpyxl import Workbook, load_workbook 
 
@@ -19,5 +21,12 @@ with open("people.csv", "r") as file:
 
 url = "https://emn178.github.io/online-tools/crc32.html"
 driver.get(url)
-time.sleep(2)
-input()
+elem = driver.find_element(By.ID, "input")
+for row in name:
+    input = f"{row[2]} {row[3]}"
+    elem.send_keys(input)
+    elem.send_keys(Keys.RETURN)
+    elem = driver.find_element(By.ID, "execute")
+    elem.send_keys(Keys.LEFT)
+    elem = driver.find_element(By.ID, "output")
+    

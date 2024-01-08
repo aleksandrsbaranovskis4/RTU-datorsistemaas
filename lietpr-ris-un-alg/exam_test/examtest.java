87adbf5cf1976e21c6bf1c7d25f9b375963cd71e
aleksandrsbaranovskis4 <aleksandrs.baranovskis_4@edu.rtu.lv>
2024-01-08 12:43:05 +0200

    aha

--- lietpr-ris-un-alg/exam_test/examtest.java
+++ lietpr-ris-un-alg/exam_test/examtest.java
@@ -1,5 +1,12 @@
 class examtest {
+    public static void print(int[] a, int i){
+        if(i < a.length){
+            System.out.print(a[i] + " ");
+            print(a, i+1);
+        }
+    }
     public static void main(String[] args){
-        
+        int[] a = {1,2,3,4,5,6,7};
+        print(a,0);
     }
 }

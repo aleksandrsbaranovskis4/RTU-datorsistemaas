8a03dd9860630f4b4af8e6edd1087377489f4c86
Aleksandrs <aleksandrs.baranovskis_4@edu.rtu.lv>
2024-10-19 10:46:54 +0300

    saving

---
+++ fizika/3_7_1.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x = np.linspace(690.7, 671.6, 623.6, 612.3, 607.3, 579.0, 577.0, 567.6, 546.1, 512.1, 496.2, 491.6, 435.8, 434.7, 433.9, 407.8, 404.7)
+print(x.size)
